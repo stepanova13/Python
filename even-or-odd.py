ad0806b24c52907e4821e30a56e4b61ba74a521f
alex <alexandra.stepanova10@gmail.com>
2022-11-11 23:22:10 -0500

    new files

---
+++ even-or-odd.py
@@ -0,0 +1,14 @@
+''' Check if a number is even or odd'''
+
+# input returns string, so we turn it into integer with int() function
+number = int(input("Please enter a number between 1-100: \n"))
+
+if number > 100:
+    print("Invalid input")
+
+else:
+    if number % 2 == 0:
+        print(f"Your number {number} is even.")
+
+    else:
+        print(f"Your number {number} is odd.")
