dfda2888b85132178cc94b152211ad064d48beb9
alex <alexandra.stepanova10@gmail.com>
2022-11-15 14:33:12 -0500

    modify english-french-bulgarian-dictionary.py

--- english-french-bulgarian-dictionary.py
+++ english-french-bulgarian-dictionary.py
@@ -20,15 +20,11 @@ english_to_bulgarian_dictionary = {
     "prune" : "слива"
     }
 
-if language == "french":
-    if word in english_to_french_dictionary:
-        print(english_to_french_dictionary.get(word))
-    else:
-        print(f"{word} is not in my memory")
-elif language == "bulgarian":
-    if word in english_to_bulgarian_dictionary:
-        print(english_to_bulgarian_dictionary.get(word))
-    else:
-        print(f"{word} is not in my memory")
-else:
+if language != "french" and language != "bulgarian":
     print("Invalid input")
+elif language == "french" and word in english_to_french_dictionary:
+    print(english_to_french_dictionary.get(word))
+elif language == "bulgarian" and word in english_to_bulgarian_dictionary:
+    print(english_to_bulgarian_dictionary.get(word))
+else:
+    print(f"{word} is not in my memory")
