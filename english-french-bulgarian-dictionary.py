f74f8430b02d95cdd2d43008889bfd2c573b3c7e
alex <alexandra.stepanova10@gmail.com>
2022-11-17 18:32:45 -0500

    changes

--- english-french-bulgarian-dictionary.py
+++ english-french-bulgarian-dictionary.py
@@ -4,7 +4,6 @@
 # create dictionary
 dictionary = {
         "english to french" :
-
         {"green" : "verte",
         "worm" : "ver",
         "glass" : "verre",
@@ -12,7 +11,6 @@ dictionary = {
         "prune" : "prune"},
 
         "english to bulgarian" :
-
         {"hi" : "здравей",
         "car" : "кола",
         "house" : "къща",
@@ -25,12 +23,14 @@ language = input("French or Bulgarian? ").lower()
 # ask user what word they want to translate
 word = input("word: ").lower()
 
+# If the language is not French or Bulgarian, print "Invalid input"
 if language != "french" and language != "bulgarian":
     print("Invalid input")
+# if language is French or Bulgarian, fetch the translated word from the dictionary
 elif language == "french" and word in dictionary["english to french"]:
     print(dictionary["english to french"].get(word))
-
 elif language == "bulgarian" and word in dictionary["english to bulgarian"]:
     print(dictionary["english to bulgarian"].get(word))
+# if word does not exist in the dictionary, print "word not in my memory"
 else:
     print(f"{word} is not in my memory")
