307e325cbc27c72344f1c978b4ecec26f7be7b28
alex <alexandra.stepanova10@gmail.com>
2022-11-17 22:28:59 -0500

    add file

--- english-french-bulgarian-dictionary.py
+++ english-french-bulgarian-dictionary.py
@@ -26,14 +26,17 @@ language = input("French or Bulgarian? ").lower()
 # ask user what word they want to translate
 word = input("word: ").lower()
 
+french = dictionary["english to french"]
+bulgarian = dictionary["english to bulgarian"]
+
 # If the language is not French or Bulgarian, print "Invalid input"
 if language != "french" and language != "bulgarian":
     print("Invalid input")
 # if language is French or Bulgarian, fetch the translated word from the dictionary
-elif language == "french" and word in dictionary["english to french"]:
-    print(dictionary["english to french"].get(word))
-elif language == "bulgarian" and word in dictionary["english to bulgarian"]:
-    print(dictionary["english to bulgarian"].get(word))
+elif language == "french" and word in french:
+    print(french.get(word))
+elif language == "bulgarian" and word in bulgarian:
+    print(bulgarian.get(word))
 # if word does not exist in the dictionary, print "word not in my memory"
 else:
     print(f"{word} is not in my memory")
---
+++ username-password-simplified-1.py
@@ -0,0 +1,32 @@
+""" Ask user for username and password 5 times. After the fifth time lock account. """
+
+# create database
+database = {
+    "credentials":
+    {
+        "username0" : "password0",
+        "username1" : "password1",
+        "username2" : "password2"
+    }
+}
+
+# assign variables
+n = 0
+username = ''
+password = ''
+
+# use while loop to keep asking until the credentials are correct or user run out of tries( 5 tries)
+while n < 5:
+    n += 1
+    username = input("username: ")
+    password = input("password: ")
+    if username in database["credentials"] and password in database["credentials"][username]:
+        print("Access granted.")
+        break
+    elif n < 5:
+        print("Incorrect. Try again.")
+    else:
+        print("Account locked.")
+
+
+
