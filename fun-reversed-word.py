160e6b64879d624fbb46a7e774c4074969a5215e
alex <alexandra.stepanova10@gmail.com>
2022-11-22 01:12:13 -0500

    add fun-reversed-word.py

---
+++ fun-reversed-word.py
@@ -0,0 +1,6 @@
+''' Fun program that asks user to type a word, and displays the reversed word '''
+
+
+word = input("Wnter a word: ")
+
+print(f"Your word is : {word}, and the reversed is: \n{word[::-1]}")
