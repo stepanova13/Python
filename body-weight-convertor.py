1eb021c4bb3f2b7c0aef4d8623a17cf3d0d7ecef
alex <alexandra.stepanova10@gmail.com>
2022-11-22 00:52:46 -0500

    add body-weihgt-convertor.py

--- body-weight-convertor.py
+++ body-weight-convertor.py
@@ -1,6 +1,9 @@
-''' Program that askes user their weight in lb and convert it in kilograms/ kg'''
+''' Program that askes user their weight in lb and converts it in kilograms/ kg'''
 
 body_weight_in_lb = int(input("What is your body weight in lb? \n"))
 body_weight_in_kg = round(body_weight_in_lb * 0.45359237, 3) # display 3 digit decimal
 
+# Display: Your body weight is ... lb,
+#          nd the equivalent in kg is: ... kg.
+#          Thank you!
 print(f"Your body weight is {body_weight_in_lb} lb,\nand the equivalent in kg is: {body_weight_in_kg} kg.\nThank you!")
