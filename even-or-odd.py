3c6468dd489e3732abf4be6b18ab3e03e4e3cffa
alex <alexandra.stepanova10@gmail.com>
2022-11-24 22:18:08 -0500

    fix even-or-odd

--- even-or-odd.py
+++ even-or-odd.py
@@ -3,7 +3,7 @@
 while True:
     # input returns string, so we cast it into integer with int() function, so that we can compare it with another integer
     number = int(input("Please enter a number between 1-100: \n"))
-    if number < 0 and number > 100:
+    if number < 0 or number > 100:
         print("Invalid input")
     else:
         if number % 2 == 0:
