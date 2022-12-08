5d4fd5e94322eadf6e089ce045de18e539e3784f
alex <alexandra.stepanova10@gmail.com>
2022-12-07 19:43:21 -0500

    modify s3.py

--- s3.py
+++ s3.py
@@ -2,26 +2,27 @@ import boto3
 import random
 import string
 
-
-def generate_random_bucket_name(y):
+def generate_random_bucket_name(prefix):
     """ 
     This function generates a unique s3 bucket name.
 
     Parameters:
-    int: user provides a integer for how many letters
+    str: bucket prefix to start the bucket name
 
     Returns:
-    str: returns a string of ascii letters joined with delimiter
-
+    str: returns a string of bucket prefix joined with 25 random numbers and characters at the end 
     """
-    return '-'.join((random.choice(string.ascii_lowercase) for x in range(y)))
+    return (prefix + str(''.join(random.choices(string.ascii_lowercase + string.digits, k=25))))
+
+bucket_prefix = 'boto3-bucket-'
 
 # call s3 client
 # allows us to interact with s3 API
 my_s3_client = boto3.client('s3')
 
+# create s3 bucket in us-east-2
 response = my_s3_client.create_bucket(
-    Bucket= generate_random_bucket_name(25),
+    Bucket= generate_random_bucket_name(bucket_prefix),
     CreateBucketConfiguration={
         'LocationConstraint': 'us-east-2',
     },
