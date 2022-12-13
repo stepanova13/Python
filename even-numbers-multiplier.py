9c34666803e549ed20d770e1d858977ef7bcbd2e
alex <alexandra.stepanova10@gmail.com>
2022-12-12 23:05:14 -0500

    add parameter to the finction

--- even-numbers-multiplier.py
+++ even-numbers-multiplier.py
@@ -7,16 +7,16 @@
 Numbers = [8, 9, 11, 20, 32, 101, 100]
 
 # defining the function
-def multiply_even_numbers():
+def multiply_even_numbers(list):
     ''' 
     this function multiplies the even numbers from a list
-    args: None
+    args: list(a list of numbers)
     returns: int
     '''
     # assigne local variable 'product' to initiate the calculation of the product
     product = 1
     # iterate over the list of numbers to take the even numbers and multiply them 
-    for i in Numbers:
+    for i in list:
         if i % 2 == 0:
             product *= i
     return product
@@ -24,7 +24,7 @@ def multiply_even_numbers():
 # body of the main
 if __name__ == '__main__':
     try:
-        print(f"The product of all even numbers in the list is: {multiply_even_numbers()}")
+        print(f"The product of all even numbers in the list is: {multiply_even_numbers(Numbers)}")
     
     # handle exceptions 
     except Exception as error:
